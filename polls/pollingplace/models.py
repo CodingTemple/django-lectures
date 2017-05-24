c7cc17153b5fc0afcae46a4aa0f106b914565bec
joel carter <joel carter>
2017-05-23 19:53:42 -0500

    Adding to polls app

--- polls/pollingplace/models.py
+++ polls/pollingplace/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
--- polls/pollingplace/urls.py
+++ polls/pollingplace/urls.py
@@ -1,10 +1,13 @@
 #Custom imports
 
 from django.conf.urls import url
+
 from . import views
 
-
-# Create your views here.
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
