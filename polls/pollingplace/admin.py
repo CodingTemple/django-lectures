1d12377b19a8ac5f8c3e307d09e9cf3fc4974206
Joel Carter <carter3689@gmail.com>
2017-05-23 22:43:26 -0500

    Adding functionality to pollingplace app

--- polls/pollingplace/admin.py
+++ polls/pollingplace/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 #importing a module that is local to this file
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Choice)
